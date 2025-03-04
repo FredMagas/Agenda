f9f602916117e5943c6eea5834121e43656a7f1e
FredMagas <fredericomaga@hotmail.com>
2025-03-04 17:47:51 -0300

    Added events table to work with agenda

--- core/admin.py
+++ core/admin.py
@@ -5,5 +5,6 @@ from .models import *
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ['usuario', 'data_evento',]
 
 admin.site.register(Evento, EventoAdmin)
